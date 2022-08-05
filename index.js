89e2daf723be34d649fabf73d074c6c153437b9a
annacarolinamekarupereira <annacarolinamekarupereira@gmail.com>
2022-08-05 23:44:21 +1000

    Completed asiigment

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(){
+    return 5+5;
+}
+
+function subtract(){
+    return 5-5;
+}
+
+function multiply(){
+    return 3*5;
+}
+
+function divide(){
+    return 15/3;
+}
+
+function add(a, b){
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function  multiply(a, b) {
+    return a*b;
+}
+
+function  divide(a, b) {
+    return a/b;
+}
+
+function  increment(n) {
+    return ++n;
+}
+
+function  decrement(n) {
+    return --n;
+}
+
+function  makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function  preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+
+
+
